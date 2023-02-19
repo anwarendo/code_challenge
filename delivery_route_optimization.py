30796c507850144577634e253cb7c254a3933124
Anwar Endris <anwarendo@gmail.com>
2023-02-19 18:42:02 +0300

    Update delivery_route_optimization.py

--- delivery_route_optimization.py
+++ delivery_route_optimization.py
@@ -1,19 +1,23 @@
-from typing import List                                         
-class Building:                                                     def __init__(self, name: str, distances: List[int]):
+#!/usr/bin/python3
+
+from typing import List
+
+class Building:
+    def __init__(self, name: str, distances: List[int]):
         self.name = name
-        self.distances = distances                              
-def delivery_route_optimization(buildings: List[Building], star\
-t: Building) -> List[Building]:
+        self.distances = distances
+
+def delivery_route_optimization(buildings: List[Building], start: Building) -> List[Building]:
     visited = set([start])
-    route = [start]                                                 current_building = start
+    route = [start]
+    current_building = start
     while len(visited) < len(buildings):
-        nearest_neighbor = None                                         nearest_distance = float('inf')
+        nearest_neighbor = None
+        nearest_distance = float('inf')
         for building in buildings:
-            if building not in visited and building.distances[c\
-urrent_building.name] < nearest_distance:
+            if building not in visited and building.distances[current_building.name] < nearest_distance:
                 nearest_neighbor = building
-                nearest_distance = building.distances[current_b\
-uilding.name]
+                nearest_distance = building.distances[current_building.name]
         visited.add(nearest_neighbor)
         route.append(nearest_neighbor)
         current_building = nearest_neighbor
