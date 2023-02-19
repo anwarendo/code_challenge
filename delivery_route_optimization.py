4e7ac17e8475803878604d3ed136358a263b383c
Neo <neo@pc>
2023-02-19 20:04:19 +0300

    Update delivery_route_optimization.py

--- delivery_route_optimization.py
+++ delivery_route_optimization.py
@@ -3,8 +3,8 @@
 from typing import List
 
 class Building:
-    def __init__(self, name: str, distances: List[int]):
-        self.name = name
+    def __init__(self, bldg_num: int, distances: List[int]):
+        self.bldg_num = bldg_num
         self.distances = distances
 
 def delivery_route_optimization(buildings: List[Building], start: Building) -> List[Building]:
@@ -15,22 +15,27 @@ def delivery_route_optimization(buildings: List[Building], start: Building) -> L
         nearest_neighbor = None
         nearest_distance = float('inf')
         for building in buildings:
-            if building not in visited and building.distances[current_building.name] < nearest_distance:
+            if building not in visited and building.distances[current_building.bldg_num] < nearest_distance:
                 nearest_neighbor = building
-                nearest_distance = building.distances[current_building.name]
+                nearest_distance = building.distances[current_building.bldg_num]
         visited.add(nearest_neighbor)
         route.append(nearest_neighbor)
         current_building = nearest_neighbor
     route.append(start)
     return route
 
-bldA = Building("A", [1000, 1400, 800])
-bldB = Building("B", [1200, 1400, 900])
-bldC = Building("C", [1700, 800, 900])
-bldStart = Building("S", [1000, 1200, 1700] )
+bldA = Building(1, [1000, 1400, 800])
+bldB = Building(2, [1200, 1400, 900])
+bldC = Building(3, [1700, 800, 900])
+bldStart = Building(0, [1000, 1200, 1700] )
 
 buildings = [bldA, bldB, bldC]
 
-print(delivery_route_optimization(buildings, bldStart))
+optimized_route = delivery_route_optimization(buildings, bldStart)
+
+
+for route in optimized_route:
+    print(f'building number: {route.bldg_num}, distance to other buildings: {route.distances}')
+    
 
 
